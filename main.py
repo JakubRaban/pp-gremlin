3d49781c80f31e4797c436786ae4d1d7e6a4ca58
Jakub <jakub.raban@gmail.com>
2021-05-22 21:35:42 +0200

    Add some queries

--- main.py
+++ main.py
@@ -1,5 +1,75 @@
-from gremlin_python.driver import client, serializer
+from gremlin_python.driver import client, serializer, resultset
 from password import password
+
+
+def print_results(callback):
+    for result in callback.results():
+        print(result)
+
+
+vertex_queries = [
+    "g.addV('person').property('id', 'thomas').property('firstName', 'Thomas').property('age', 44).property('pk', 'pk')",
+    "g.addV('person').property('id', 'mary').property('firstName', 'Mary').property('lastName', 'Andersen').property('age', 39).property('pk', 'pk')",
+    "g.addV('person').property('id', 'ben').property('firstName', 'Ben').property('lastName', 'Miller').property('pk', 'pk')",
+    "g.addV('person').property('id', 'robin').property('firstName', 'Robin').property('lastName', 'Wakefield').property('pk', 'pk')",
+    "g.addV('device').property('id', 'device1').property('type', 'pc').property('cores', 8).property('pk', 'pk')",
+    "g.addV('device').property('id', 'device2').property('type', 'pc').property('cores', 4).property('pk', 'pk')",
+    "g.addV('device').property('id', 'device3').property('type', 'smartphone').property('cores', 4).property('pk', 'pk')",
+    "g.addV('device').property('id', 'device4').property('type', 'smartphone').property('cores', 4).property('pk', 'pk')",
+    "g.addV('device').property('id', 'device5').property('type', 'smartphone').property('cores', 2).property('pk', 'pk')",
+    "g.addV('device').property('id', 'device6').property('type', 'tablet').property('cores', 2).property('pk', 'pk')",
+    "g.addV('os').property('id', 'win10').property('name', 'windows').property('version', '10').property('pk', 'pk')",
+    "g.addV('os').property('id', 'ubuntu20').property('name', 'ubuntu').property('version', '20').property('pk', 'pk')",
+    "g.addV('os').property('id', 'android').property('name', 'android').property('pk', 'pk')",
+]
+
+edge_queries = [
+    "g.V('device1').addE('hasOs').to(g.V('win10'))",
+    "g.V('device1').addE('hasOs').to(g.V('ubuntu20'))",
+    "g.V('device2').addE('hasOs').to(g.V('win10'))",
+    "g.V('device3').addE('hasOs').to(g.V('android'))",
+    "g.V('device4').addE('hasOs').to(g.V('android'))",
+    "g.V('device5').addE('hasOs').to(g.V('android'))",
+    "g.V('device6').addE('hasOs').to(g.V('android'))",
+    "g.V('thomas').addE('hasDevice').to(g.V('device1'))",
+    "g.V('thomas').addE('hasDevice').to(g.V('device3'))",
+    "g.V('mary').addE('hasDevice').to(g.V('device2'))",
+    "g.V('ben').addE('hasDevice').to(g.V('device4'))",
+    "g.V('robin').addE('hasDevice').to(g.V('device5'))",
+    "g.V('robin').addE('hasDevice').to(g.V('device6'))",
+    "g.V('mary').addE('knows').to(g.V('thomas'))",
+    "g.V('mary').addE('knows').to(g.V('robin'))",
+    "g.V('robin').addE('knows').to(g.V('ben'))",
+]
+
+
+def cleanup_db():
+    cosmos_client.submit('g.V().drop()')
+
+
+def setup_db():
+    for query in vertex_queries + edge_queries:
+        cosmos_client.submit(query)
+
+
+def find_people_with_more_than_one_device():
+    return cosmos_client.submit("g.V().hasLabel('person').where(__.out('hasDevice').count().is(gt(1)))")
+
+
+def find_devices_with_4_cores_or_less():
+    return cosmos_client.submit("g.V().hasLabel('device').has('cores', lte(4))")
+
+
+def find_people_who_know_someone_with_windows_device():
+    return cosmos_client.submit("g.V().hasLabel('person').where(__.out('knows').out('hasDevice').out('hasOs').has('name', 'windows'))")
+
+
+def update_ubuntu_to_21():
+    return cosmos_client.submit("g.V().hasLabel('os').has('name', 'ubuntu').property('version', '21')")
+
+
+def kill_people_who_know_no_one():
+    return cosmos_client.submit("g.V().hasLabel('person').where(__.out('knows').count().is(eq(0))).drop()")
 
 
 cosmos_client = client.Client('wss://zuz.gremlin.cosmosdb.azure.com:443/', 'g',
@@ -8,12 +78,13 @@ cosmos_client = client.Client('wss://zuz.gremlin.cosmosdb.azure.com:443/', 'g',
                               message_serializer=serializer.GraphSONSerializersV2d0()
                               )
 
+cleanup_db()
+setup_db()
 
-def cleanup_db():
-    cosmos_client.submit('g.V().drop()')
+print_results(find_people_with_more_than_one_device())
+print_results(find_devices_with_4_cores_or_less())
+print_results(find_people_who_know_someone_with_windows_device())
+update_ubuntu_to_21()
+kill_people_who_know_no_one()
 
-
-callback = client.submit("g.addV('person').property('id', 'thomas.1').property('firstName', 'Thomas').property('lastName', 'Andersen').property('age', 44)")
-print(callback)
-client.close()
-
+cosmos_client.close()
