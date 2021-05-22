f1b8a59e076a44531716266196d8547589559e34
Jakub <jakub.raban@gmail.com>
2021-05-22 12:35:39 +0200

    remove config

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from gremlin_python.driver import client, serializer
+from password import password
 
 client = client.Client('wss://zuz.gremlin.cosmosdb.azure.com:443/', 'g',
                        username="/dbs/graphdb/colls/Persons",
-                       password="",
+                       password=password,
                        message_serializer=serializer.GraphSONSerializersV2d0()
                        )
 callback = client.submit("g.addV('person').property('id', 'thomas.1').property('firstName', 'Thomas').property('lastName', 'Andersen').property('age', 44)")
